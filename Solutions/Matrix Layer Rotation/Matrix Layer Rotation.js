14ab564abe845a1395a0b8ff87d011d2b7db3e0c
LeoNiz <leo_nd@hotmail.fr>
2018-11-01 14:57:08 +0100

    Update Matrix Layer Rotation.js

--- Solutions/Matrix Layer Rotation/Matrix Layer Rotation.js
+++ Solutions/Matrix Layer Rotation/Matrix Layer Rotation.js
@@ -1,9 +1,9 @@
 // Complete the matrixRotation function below.
 function matrixRotation(matrix, r) {
-    var m = matrix.length, n = matrix[0].length;
-    var rotate = Array(m).fill().map(_ => Array(n).fill());
-    var frame_c_max = Math.min(m,n)/2;
-    var list = Array(frame_c_max).fill().map(_ => []);
+    var m = matrix.length, n = matrix[0].length; // initiate m and n
+    var rotate = Array(m).fill().map(_ => Array(n).fill()); // rotate matrix
+    var frame_c_max = Math.min(m,n)/2; // maximum number of frame
+    var list = Array(frame_c_max).fill().map(_ => []); // initiate list array that contains values for each frame
 
     for(var i=0; i<m; i++){
         for(var j=0; j<n; j++){
@@ -24,8 +24,8 @@ function matrixRotation(matrix, r) {
     function param(i,j){
         var ii = i >= m/2 ? (m-1) - i : i;
         var jj = j >= n/2 ? (n-1) - j : j;
-        var frame_v = Math.min(ii,jj);
-        var frame_c = 2 * (m+n-2) - 8 * frame_v;
+        var frame_v = Math.min(ii,jj); // determine on which frame is (i,j)
+        var frame_c = 2 * (m+n-2) - 8 * frame_v; // the number of elements that frame_v contains
         var mij = i + j - 2*frame_v;
         if(!(j == frame_v || i == (m-1) - frame_v)) mij = frame_c - mij;
         return {
