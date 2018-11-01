eed48c4451ad4818ed174772b7bb1a4aee309b82
LeoNiz <leo_nd@hotmail.fr>
2018-11-01 14:12:18 +0100

    Create Matrix Layer Rotation.js

---
+++ Solutions/Matrix Layer Rotation/Matrix Layer Rotation.js
@@ -0,0 +1,37 @@
+// Complete the matrixRotation function below.
+function matrixRotation(matrix, r) {
+    var m = matrix.length, n = matrix[0].length;
+    var rotate = Array(m).fill().map(_ => Array(n).fill());
+    var frame_c_max = Math.min(m,n)/2;
+    var list = Array(frame_c_max).fill().map(_ => []);
+
+    for(var i=0; i<m; i++){
+        for(var j=0; j<n; j++){
+            var p = param(i,j);
+            list[p.frame_v][(p.mij+r)%p.frame_c] = matrix[i][j];
+        }
+    }
+    for(var i=0; i<m; i++){
+        for(var j=0; j<n; j++){
+            var p = param(i,j);
+            rotate[i][j] = list[p.frame_v][p.mij];
+        }
+    }
+    rotate.forEach(row => {
+        console.log(row.join(" "));
+    })
+    
+    function param(i,j){
+        var ii = i >= m/2 ? (m-1) - i : i;
+        var jj = j >= n/2 ? (n-1) - j : j;
+        var frame_v = Math.min(ii,jj);
+        var frame_c = 2 * (m+n-2) - 8 * frame_v;
+        var mij = i + j - 2*frame_v;
+        if(!(j == frame_v || i == (m-1) - frame_v)) mij = frame_c - mij;
+        return {
+            frame_v : frame_v,
+            frame_c : frame_c,
+            mij : mij
+        }
+    }
+}
